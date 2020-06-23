7ca586979a124b806e535951e72e0c2d41999f95
xuezhen <13920922817@163.com>
2020-06-23 16:46:23 +0800

    111

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    configureWebpack:{
+        devServer:{
+            port:8089,//端口号
+            open:true,
+            //mock 数据
+            before(app){
+
+            }
+        }
+    }
+}
